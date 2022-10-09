5e689eb7029082db3728f167e1da71a95b29612b
Natalya Maximova <natka-31@mail.ru>
2022-10-09 19:55:07 +0500

    Добавить компонент Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import './Header.css'
-import logo from '../../images/logo.svg' 
+import logo from '../../images/logo.svg'
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => {
    return (
       <header className="header">
          <img className="header__logo" src={logo} alt="Логотип" />
+         <Navigation />
       </header>
    )
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+import './Navigation.css'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+   return (
+      <div className="navigation">
+         <Link to="/sign-up" className="navigation__link">Регистрация</Link>
+         <Link to="/signin">
+            <button className="navigation__button">
+               Войти
+            </button>
+         </Link>
+      </div>
+   )
+}
+
+export default Navigation;
+
+// Navigation — компонент, который отвечает за меню навигации на сайте.
