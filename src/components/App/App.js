7a6b08506e6a3cbeef034faa72e8ed9b25e8e9d0
Natalya Maximova <natka-31@mail.ru>
2022-10-09 15:42:03 +0500

    Добавить компонент Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from '../Main';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Main />
+      <Footer/>
     </div>
   );
 }
