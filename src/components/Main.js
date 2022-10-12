a4033acf59f6d28cb9259ed7c23eb1e65be19340
Natalya Maximova <natka-31@mail.ru>
2022-10-12 13:31:41 +0500

    Добавить компонент Movies

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,21 @@
-import Promo from './Promo/Promo';
-import AboutProject from './AboutProject/AboutProject';
-import Techs from './Techs/Techs';
-import AboutMe from './AboutMe/AboutMe';
-import Portfolio from './Portfolio/Portfolio';
+// import Promo from './Promo/Promo';
+// import AboutProject from './AboutProject/AboutProject';
+// import Techs from './Techs/Techs';
+// import AboutMe from './AboutMe/AboutMe';
+// import Portfolio from './Portfolio/Portfolio';
+
+import Movies from "./Movies/Movies";
+
 
 const Main = () => {
    return (
       <>
-         <Promo />
+         {/* <Promo />
          <AboutProject />
          <Techs />
          <AboutMe />
-         <Portfolio />
+         <Portfolio /> */}
+         <Movies />
       </>
    );
 }
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,12 @@
+import './Movies.css';
+
+const Movies = () => {
+   return (
+      <section className="movies">
+      </section>
+   )
+}
+
+export default Movies;
+
+// Movies — компонент страницы с поиском по фильмам.
