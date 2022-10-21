fe3824c14dace20b318931dd4f909fe4b7eb267d
Natalya Maximova <natka-31@mail.ru>
2022-10-21 19:56:28 +0500

    Добавить компонент CurrentUserContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 import { Switch, Route } from 'react-router-dom';
 import Main from '../Main';
 import Movies from '../Movies/Movies';
@@ -10,43 +11,49 @@ import Register from '../Register/Register';
 import Profile from '../Profile/Profile';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
+
 function App() {
   // временное решение, пока нет функции изменения состояния
   // eslint-disable-next-line
   const [loggedIn, setLoggedIn] = useState(true);
+   // eslint-disable-next-line
+  const [currentUser, setCurrentUser] = useState({});
 
   return (
-    <div className='App'>
-      <Switch>
-        <Route exact path='/'>
-          <Main />
-        </Route>
-        <ProtectedRoute
-          path='/movies'
-          component={Movies}
-          loggedIn={loggedIn}
-        />
-        <ProtectedRoute
-          path='/saved-movies'
-          component={SavedMovies}
-          loggedIn={loggedIn}
-        />
-        <ProtectedRoute
-          path='/profile'
-          component={Profile}
-          loggedIn={loggedIn}
-        />
-        <Route path='/signin'>
-          <Login />
-        </Route>
-        <Route path='/signup'>
-          <Register />
-        </Route>
-        <Route path='*'>
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <ProtectedRoute
+            path='/movies'
+            component={Movies}
+            loggedIn={loggedIn}
+          />
+          <ProtectedRoute
+            path='/saved-movies'
+            component={SavedMovies}
+            loggedIn={loggedIn}
+          />
+          <ProtectedRoute
+            path='/profile'
+            component={Profile}
+            loggedIn={loggedIn}
+          />
+          <Route path='/signin'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Register />
+          </Route>
+          <Route path='*'>
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </div>
+  </CurrentUserContext.Provider>
+    
   );
 }
 
---
+++ src/contexts/CurrentUserContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CurrentUserContext = React.createContext(); 
+
+// CurrentUserContext — компонент для обмена данными между компонентами приложения.
