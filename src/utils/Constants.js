164774d43f6dd8800a358a7b61f6fd0ff67f66df
Natalya Maximova <natka-31@mail.ru>
2022-11-06 01:49:31 +0500

    Изменить адрес фронта

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,4 @@
-const { NODE_ENV } = process.env
-export const BASE_URL = (NODE_ENV === 'production') ? 'https://api.domainname.nataly.nomoredomains.icu' : 'http://localhost:3000';
-// export const BASE_URL = 'https://api.domainname.nataly.nomoredomains.icu';
+export const BASE_URL = 'https://api.domainname.nataly.nomoredomains.icu';
 export const MOVIE_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export const checkResponse = (res) => {
